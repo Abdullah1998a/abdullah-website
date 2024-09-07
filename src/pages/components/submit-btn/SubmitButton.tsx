69639e7c56c28c965dd1da5c461deb458b816199
Abdullah Alnoime <abdullahalnoime@gmail.com>
2024-09-07 20:31:14 +0300

    update

--- src/pages/components/submit-btn/SubmitButton.tsx
+++ src/pages/components/submit-btn/SubmitButton.tsx
@@ -10,19 +10,19 @@ type ValuesType = {
 };
 export default function SubmitButton({ formik }: {formik: FormikContextType<ValuesType>}) {
   const { t } = useTranslation();
-  const disabled = !formik.isValid || formik.isSubmitting;
+  const disabled = !formik.isValid;
   return (
     <motion.button
       variants={popupVariants}
-      className="button w-full sm:w-fit mx-auto disabled:bg-red-600 disabled:text-white dark:disabled:bg-red-500 disabled:shadow-none flex justify-center items-center gap-2"
+      className={`button w-full sm:w-fit mx-auto disabled:bg-red-600 disabled:text-white dark:disabled:bg-red-500 disabled:shadow-none flex justify-center items-center gap-3 ${ formik.isSubmitting ? "text-gray-600 dark:text-neutral-100 bg-gray-300 dark:bg-neutral-800 pointer-events-none" : null }`}
       disabled={disabled}
       type="submit"
     >
     { formik.isSubmitting ? (
         <>
-          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          <svg className="animate-spin h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="stroke-white dark:stroke-neutral-600" cx="12" cy="12" r="10" strokeWidth="4"></circle>
+            <path className="fill-primary dark:fill-dark" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
           </svg>
           {t("contact.form_labels.send_btn.1")}
         </>
--- src/pages/components/success/Success.tsx
+++ src/pages/components/success/Success.tsx
@@ -1,6 +1,5 @@
 import { backdropVariants, modalVariants } from "./variants";
 import { Dispatch, SetStateAction, useEffect } from "react";
-import done from "../../assets/images/done.svg";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./success.css";
@@ -28,7 +27,7 @@ export default function Success({ thank, setThank }: SuccessProbs) {
       setThank(null);
       navigate("/");
       window.scrollTo(0, 0);
-    }, 2500);
+    }, 2000);
     return () => clearInterval(counterID);
   }, []);
   return (
@@ -40,14 +39,16 @@ export default function Success({ thank, setThank }: SuccessProbs) {
       className="backdrop"
     >
       <motion.div variants={modalVariants} className="success-modal">
-        <img src={done} className="w-16 mx-auto" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-20 mx-auto">
+          <path className="fill-green-800 dark:fill-green-500" fillRule="evenodd" d="M12 21a9 9 0 0 0 7.51-13.961l-7.155 7.95a2 2 0 0 1-2.687.262L6.4 12.8a1 1 0 0 1 1.2-1.6l3.268 2.451l7.346-8.161A9 9 0 1 0 12 21" clipRule="evenodd"/>
+        </svg>
         <h1 className="text-lg dark:text-neutral-300">
           {head}{" "}
           <span className="capitalize text-primary dark:text-dark rtl:font-ibmBold">
            {name}
           </span>
         </h1>
-        <p className="text-slate-800 mt-3 dark:text-neutral-300 rtl:font-ibmRegular">
+        <p className="text-slate-800 dark:text-neutral-300 rtl:font-ibmRegular">
           {body}
         </p>
       </motion.div>
