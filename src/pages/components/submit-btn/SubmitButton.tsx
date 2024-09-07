38d81cf4add0c68291c6b241f77903402035f9f1
Abdullah Alnoime <138254438+Abdullah1998a@users.noreply.github.com>
2024-09-07 16:39:17 +0300

    Update SubmitButton.tsx

--- src/pages/components/submit-btn/SubmitButton.tsx
+++ src/pages/components/submit-btn/SubmitButton.tsx
@@ -18,21 +18,34 @@ export default function SubmitButton({ formik }: {formik: FormikContextType<Valu
       disabled={disabled}
       type="submit"
     >
-      {t("contact.form_labels.send_btn")}{" "}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        className="rtl:rotate-180"
-      >
-        <path
-          className={`fill-white ${disabled ? "dark:fill-white" : "dark:fill-neutral-900"}`}
-          fillRule="evenodd"
-          d="M3.402 6.673c-.26-2.334 2.143-4.048 4.266-3.042l11.944 5.658c2.288 1.083 2.288 4.339 0 5.422L7.668 20.37c-2.123 1.006-4.525-.708-4.266-3.042L3.882 13H12a1 1 0 1 0 0-2H3.883z"
-          clipRule="evenodd"
-        />
-      </svg>
+    { formik.isSubmitting ? (
+        <>
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          {t("contact.form_labels.send_btn.1")}
+        </>
+      ) : (
+        <>
+          {t("contact.form_labels.send_btn.0")}{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            className="rtl:rotate-180"
+          >
+            <path
+              className={`fill-white ${disabled ? "dark:fill-white" : "dark:fill-neutral-900"}`}
+              fillRule="evenodd"
+              d="M3.402 6.673c-.26-2.334 2.143-4.048 4.266-3.042l11.944 5.658c2.288 1.083 2.288 4.339 0 5.422L7.668 20.37c-2.123 1.006-4.525-.708-4.266-3.042L3.882 13H12a1 1 0 1 0 0-2H3.883z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </>
+      )
+    }
     </motion.button>
   );
 }
