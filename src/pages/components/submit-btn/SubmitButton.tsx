2d8cd3f5b1a2324ee93a2226ae94d7e67b99064e
Abdullah Alnoime <138254438+Abdullah1998a@users.noreply.github.com>
2024-09-07 16:10:46 +0300

    Update SubmitButton.tsx

--- src/pages/components/submit-btn/SubmitButton.tsx
+++ src/pages/components/submit-btn/SubmitButton.tsx
@@ -10,11 +10,12 @@ type ValuesType = {
 };
 export default function SubmitButton({ formik }: {formik: FormikContextType<ValuesType>}) {
   const { t } = useTranslation();
+  const disabled = !formik.isValid || formik.isSubmitting;
   return (
     <motion.button
       variants={popupVariants}
-      className="button w-full sm:w-fit mx-auto disabled:bg-red-600 dark:disabled:bg-red-500 disabled:shadow-none flex justify-center items-center gap-2"
-      disabled={!formik.isValid}
+      className="button w-full sm:w-fit mx-auto disabled:bg-red-600 disabled:text-white dark:disabled:bg-red-500 disabled:shadow-none flex justify-center items-center gap-2"
+      disabled={disabled}
       type="submit"
     >
       {t("contact.form_labels.send_btn")}{" "}
@@ -26,7 +27,7 @@ export default function SubmitButton({ formik }: {formik: FormikContextType<Valu
         className="rtl:rotate-180"
       >
         <path
-          className="fill-white"
+          className="fill-white dark:fill-neutral-900"
           fillRule="evenodd"
           d="M3.402 6.673c-.26-2.334 2.143-4.048 4.266-3.042l11.944 5.658c2.288 1.083 2.288 4.339 0 5.422L7.668 20.37c-2.123 1.006-4.525-.708-4.266-3.042L3.882 13H12a1 1 0 1 0 0-2H3.883z"
           clipRule="evenodd"
