907e7efcfb347b7892f60345e93597071a024409
Abdullah Alnoime <138254438+Abdullah1998a@users.noreply.github.com>
2024-09-07 16:20:22 +0300

    Update SubmitButton.tsx

--- src/pages/components/submit-btn/SubmitButton.tsx
+++ src/pages/components/submit-btn/SubmitButton.tsx
@@ -27,7 +27,7 @@ export default function SubmitButton({ formik }: {formik: FormikContextType<Valu
         className="rtl:rotate-180"
       >
         <path
-          className={ disabled ? "dark:fill-white" : "fill-white dark:fill-neutral-900"
+          className={`fill-white ${disabled ? "dark:fill-white" : "dark:fill-neutral-900"}`}
           fillRule="evenodd"
           d="M3.402 6.673c-.26-2.334 2.143-4.048 4.266-3.042l11.944 5.658c2.288 1.083 2.288 4.339 0 5.422L7.668 20.37c-2.123 1.006-4.525-.708-4.266-3.042L3.882 13H12a1 1 0 1 0 0-2H3.883z"
           clipRule="evenodd"
